8c63859c50c13d7c5df7f2ae461d9f7fe4b8c945
HeshSs <hishmat.salehi2015@gmail.com>
2019-10-30 00:10:00 -0400

    Almost done Assign3, I just gotta work on polyToPolyList and PolyListToPoly

--- Assign_3.hs
+++ Assign_3.hs
@@ -7,7 +7,7 @@ module Assign_3 where
 import qualified Data.Map.Strict as IM
 
 macid :: String
-macid = "salehh6"
+macid = "Salehh6"
 
 data Poly a = X
             | Coef a
@@ -23,43 +23,58 @@ newtype PolyList a = PolyList [a]
 {- -----------------------------------------------------------------
  - polyValue
  - -----------------------------------------------------------------
- - Description: TODO add comments on polyValue here
+ - Description: polyValue p n is the value of p at n
  -}
 polyValue :: Num a => Poly a -> a -> a
-polyValue p n = error "TODO: define me"
+polyValue X n = n
+polyValue (Coef a) _ = a
+polyValue (Sum a b) n = polyValue a n + polyValue b n
+polyValue (Prod a b) n = polyValue a n * polyValue b n
 
 {- -----------------------------------------------------------------
  - polyListValue
  - -----------------------------------------------------------------
- - Description: TODO add comments on polyListValue here
+ - Description: polyListValue p1 n is the value of the PolyList p1 at n
  -}
 polyListValue :: (Num a,Eq a) => PolyList a -> a -> a
-polyListValue p1 n = error "TODO: define me"
+polyListValue (PolyList p1) n = if p1 == []
+                                then 0
+                                    else ((p1 !! lastIndex) * (n ^ lastIndex)) + polyListValue (PolyList (take lastIndex p1)) n
+                                        where lastIndex = (length p1 - 1)
 
 {- -----------------------------------------------------------------
  - polyListSum
  - -----------------------------------------------------------------
- - Description: TODO add comments on polyListSum here
+ - Description: Returns the sum of the polynomials represented by p1 and q1.
  -}
 polyListSum :: (Num a,Eq a) => PolyList a -> PolyList a -> PolyList a
-polyListSum p1 q1 = error "TODO: define me"
-
+polyListSum (PolyList p1) (PolyList q1) = if length p1 > length q1
+                                          then PolyList ((zipWith (+) p1 q1) ++ (drop (length q1) p1))
+                                             else PolyList ((zipWith (+) p1 q1) ++ (drop (length p1) q1))
 
 {- -----------------------------------------------------------------
  - polyListDegree
  - -----------------------------------------------------------------
- - Description: TODO add comments on polyListDegree here
+ - Description: Returns the degree of the polynomial represented by p1.
  -}
 polyListDegree :: (Num a,Eq a) => PolyList a -> Integer
-polyListDegree p1 = error "TODO: define me"
+polyListDegree (PolyList p1) = if p1 == [] || (zipWith (+) p1 p1) == p1
+                               then 0
+                                   else toInteger (length p1 - 1)
 
 {- -----------------------------------------------------------------
  - polyListProd
  - -----------------------------------------------------------------
- - Description: TODO add comments on polyListProd here
+ - Description: Returns the product of the polynomials represented by p1 and q1.
  -}
 polyListProd :: (Num a,Eq a) => PolyList a -> PolyList a -> PolyList a
-polyListProd p1 q1 = error "TODO: define me"
+polyListProd (PolyList p1) (PolyList q1) = if p1 == [] || q1 == []
+                                           then PolyList []
+                                               else if length p1 > length q1
+                                                    then polyListSum (PolyList ((replicate lastIndexQ 0) ++ (map (*(q1 !! lastIndexQ)) p1))) (polyListProd (PolyList p1) (PolyList (take lastIndexQ q1)))
+                                                        else polyListSum (PolyList ((replicate lastIndexP 0) ++ (map (*(p1 !! lastIndexP)) q1))) (polyListProd (PolyList (take lastIndexP p1)) (PolyList q1))
+                                                            where lastIndexP = (length p1 - 1)
+                                                                  lastIndexQ = (length q1 - 1)
 
 {- -----------------------------------------------------------------
  - polyListToPoly
